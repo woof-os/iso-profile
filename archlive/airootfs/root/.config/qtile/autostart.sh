f422f29c53e6ee0c118f2ca4fb698fc7e3ee2493
ACuteWoof <sutharshanvithushan@gmail.com>
2024-05-10 18:43:27 +0530

    calamares to root

--- archlive/airootfs/root/.config/qtile/autostart.sh
+++ archlive/airootfs/root/.config/qtile/autostart.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 picom &
-# protonvpn-cli connect -f -p tcp &
 xsetroot -cursor_name left_ptr &
 nm-applet &
 /usr/bin/ibus-daemon -dr &
-# calamares &
+ calamares &
 # noisetorch &
 # flameshot &
 xfce4-power-manager &
